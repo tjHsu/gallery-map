a2c337a99186e9c4ded3669264e5731e21f4e41b
Ting-Jui Hsu <ting-jui.hsu@klarna.com>
2022-08-30 00:10:13 +0200

    Update galleries constants

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Header: React.FC = () => {
   return (
     <div className="navbar bg-base-100">
-      <a className="btn btn-ghost normal-case text-xl">Taipei Gallery Map</a>
+      <a className="btn btn-ghost normal-case text-3xl">Taipei Gallery Map</a>
     </div>
   )
 }
--- src/constants/Constants.ts
+++ src/constants/Constants.ts
@@ -1,14 +1,102 @@
 export const GALLERIES = [
   {
-    name: "Whitestone Gallery 白石畫廊",
-    website: "https://www.whitestone-gallery.com/"
+    name: "333GALLERY 333畫廊",
+    website: "https://www.instagram.com/333gallery.tw/",
+  },
+  {
+    name: "ASIA ART CENTER 亞洲藝術中心",
+    website: "http://www.asiaartcenter.org/asia/?lang=en",
+  },
+  {
+    name: "Bluerider ART Gallery 藍騎士藝術空間",
+    website: "https://blueriderart.com/en/",
+  },
+  {
+    name: "CHINI GALLERY 采泥藝術",
+    website: "https://www.chinigallery.com/",
+  },
+  {
+    name: "DOPENESS ART LAB",
+    website: "https://www.instagram.com/dopenessartlab/"
+  },
+  {
+    name: "Double Square Gallery 双方藝廊",
+    website: "https://www.doublesquare.com.tw/en",
+  },
+  {
+    name: "Each Modern 亞紀畫廊",
+    website: "https://eachmodern.com/en/",
+  },
+  {
+    name: "EQUAL 等號",
+    website: "http://www.facebook.com/equalartlab",
+  },
+  {
+    name: "ESLITE GALLERY 誠品畫廊",
+    website: "https://www.eslitegallery.com/en/",
+  },
+  {
+    name: "isart Gallery 御書房藝廊",
+    website: "https://www.isart.tw/en",
+  },
+  {
+    name: "Liang Gallery 尊彩藝術中心",
+    website: "http://www.lianggallery.com/en/"
+  },
+  {
+    name: "Lin & Lin Gallery 大未來林舍畫廊",
+    website: "http://www.linlingallery.com/eng/",
+  },
+  {
+    name: "Michael Ku Gallery 谷公館",
+    website: "https://michaelkugallery.com/en/home/",
+  },
+  {
+    name: "Mind Set Art Center 安卓藝術",
+    website: "https://www.art-msac.com/en/",
+  },
+  {
+    name: "nca | nichido contemporary art 日動畫廊當代館",
+    website: "https://www.nca-g.com/en/"
+  },
+  {
+    name: "Nunu Fine Art 路由藝術",
+    website: "https://www.nunufineart.com/"
+  },
+  {
+    name: "Powen Gallery 紅野畫廊",
+    website: "https://powengallery.com/index.php/en/"
+  },
+  {
+    name: "Project Fulfill Art Space 就在藝術空間",
+    website: "https://www.projectfulfill.com/"
+  },
+  {
+    name: "PTT Space 八大畫廊",
+    website: "https://www.pttspace.com/?lang=en"
+  },
+  {
+    name: "Soka Art 索卡藝術",
+    website: "http://soka-art.com/en"
   },
   {
     name: "Tina Keng Gallery 耿畫廊",
     website: "https://www.tinakenggallery.com/en/"
   },
   {
-    name: "Liang Gallery 尊彩藝術中心",
-    website: "http://www.lianggallery.com/en/"
+    name: "TING TING ART SPACE 丁丁藝術空間",
+    website: "https://tingtingartspace.com/"
   },
+  {
+    name: "TKG+",
+    website: "https://www.tkgplus.com/en/"
+  },
+  {
+    name: "Whitestone Gallery 白石畫廊",
+    website: "https://www.whitestone-gallery.com/"
+  },
+  {
+    name: "YIR ARTS 伊日藝術計劃",
+    website: "https://yiriarts.com.tw/"
+  }
 ]
