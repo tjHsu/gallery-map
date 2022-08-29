b5eb4b621f6bc3f5df3d415de65f4e50ad6c362f
Ting-Jui Hsu <ting-jui.hsu@klarna.com>
2022-08-29 23:11:58 +0200

    Update Footer colour

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer: React.FC = () => {
   return (
-    <footer className="footer items-center p-4 bg-neutral text-neutral-content">
+    <footer className="footer items-center p-4 bg-base-300 text-base-content">
       <div className="items-center grid-flow-col">
         <p>Copyright © 2022 - All right reserved</p>
       </div>
