184aa7fe75aeaa257fd855558e2947e6eca56c95
Ting-Jui Hsu <ting-jui.hsu@klarna.com>
2022-08-31 00:28:41 +0200

    Fix svg file

--- src/pages/galleries/[name].tsx
+++ src/pages/galleries/[name].tsx
@@ -44,7 +44,6 @@ const Gallery: React.FC<GalleryDetail> = (props) => {
           <tbody>
             <tr>
               <td className='text-base'>Exhibitions</td>
-              <td className='text-base'>{exhibitions.length!! ? '':'N/A' }</td>
             </tr>
             {exhibitions?.map((exhibition: Exhibition) =>
               <ExhibitionItem key={exhibition.showName} artist={exhibition.artist} showName={exhibition.showName} date={exhibition.date} openingDate={exhibition.openingDate} />
@@ -59,7 +58,7 @@ const Gallery: React.FC<GalleryDetail> = (props) => {
               <td>website</td>
               <td>
                 <a href={`${props.website}`} target="_blank" rel="noreferrer">
-                  <svg width="24" height="24" viewBox="0 0 24 24" stroke-width="1.5" fill="none" xmlns="http://www.w3.org/2000/svg"> <path d="M21 3L15 3M21 3L12 12M21 3V9" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" /> <path d="M21 13V19C21 20.1046 20.1046 21 19 21H5C3.89543 21 3 20.1046 3 19V5C3 3.89543 3.89543 3 5 3H11" stroke="currentColor" stroke-linecap="round" />
+                  <svg width="24" height="24" viewBox="0 0 24 24" strokeWidth="1.5" fill="none" xmlns="http://www.w3.org/2000/svg"> <path d="M21 3L15 3M21 3L12 12M21 3V9" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" /> <path d="M21 13V19C21 20.1046 20.1046 21 19 21H5C3.89543 21 3 20.1046 3 19V5C3 3.89543 3.89543 3 5 3H11" stroke="currentColor" strokeLinecap="round" />
                   </svg>
                 </a>
               </td>
