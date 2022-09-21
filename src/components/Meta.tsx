07fc4e9be77cc9389c9abbbe80c8562b31e674a5
Ting-Jui Hsu <ting-jui.hsu@klarna.com>
2022-09-22 00:53:22 +0200

    update themed colour

--- src/components/Meta.tsx
+++ src/components/Meta.tsx
@@ -27,9 +27,9 @@ const Meta = () => {
         color="#000000"
       /> */}
       <link rel="shortcut icon" href="/favicon/favicon.ico" />
-      <meta name="msapplication-TileColor" content="#000000" />
+      <meta name="msapplication-TileColor" content="#5D13E7" />
       {/* <meta name="msapplication-config" content="/favicon/browserconfig.xml" /> */}
-      <meta name="theme-color" content="#000" />
+      <meta name="theme-color" content="#5D13E7" />
       {/* <link rel="alternate" type="application/rss+xml" href="/feed.xml" /> */}
       <meta
         name="description"
