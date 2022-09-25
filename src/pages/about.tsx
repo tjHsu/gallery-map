a595844712205c3cc7d9fa63e27b2fc8f27784a1
Ting-Jui Hsu <ting-jui.hsu@klarna.com>
2022-09-25 15:44:29 +0200

    Update about us page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -22,6 +22,15 @@ const About: NextPage = () => {
               </td>
             </tr>
             <tr>
+                <td className="text-base align-top">Contact us</td>
+                <td>
+                  <a href='https://form.typeform.com/to/yovdmQxU' target="_blank" rel="noreferrer">
+                    <svg width="24" height="24" viewBox="0 0 24 24" strokeWidth="1.5" fill="none" xmlns="http://www.w3.org/2000/svg"> <path d="M21 3L15 3M21 3L12 12M21 3V9" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" /> <path d="M21 13V19C21 20.1046 20.1046 21 19 21H5C3.89543 21 3 20.1046 3 19V5C3 3.89543 3.89543 3 5 3H11" stroke="currentColor" strokeLinecap="round" />
+                    </svg>
+                  </a>
+                </td>
+              </tr>
+            <tr>
               <td></td>
             </tr>
           </tbody>
