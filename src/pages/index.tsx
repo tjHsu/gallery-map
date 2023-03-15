b94cfcde2eecd195ba81334eec97431ececd2a61
Ting <ting@Tings-MacBook-Pro.local>
2023-03-15 14:09:10 +0100

    Added Mixpanel and woopra

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from "next"
 import Link from 'next/link'
 import Smartlook from 'smartlook-client'
+import mixpanel from 'mixpanel-browser'
 import { useEffect } from "react"
 
 const LandingPage: NextPage = () => {
@@ -8,6 +9,8 @@ const LandingPage: NextPage = () => {
     // Client-side-only code
     Smartlook.init('bcd622dee28566231097b08a614f2ac2a4c1de02')
 })
+mixpanel.init('3964f725cfe2946b193963b4f3d4938c', {debug: true, ignore_dnt: true}); 
+mixpanel.track('Landing Page');
   return (
     <div className='ml-5'>
       <div className="overflow-x-auto">
