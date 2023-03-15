df7f21bf9c6cf66f20b137dab6ea471968a68449
Ting <ting@Tings-MacBook-Pro.local>
2023-03-15 12:25:42 +0100

    Revert "Added smartlook script"
    This reverts commit 7565f7eede1796879b6ea45d14c6bf868ee731ba.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,14 +6,7 @@ const MyApp: AppType = ({ Component, pageProps }) => {
 
   return (
     <Layout>
-      <script type='text/javascript'>
-        window.smartlook||(function(d) {
-          var o=smartlook=function(){o.api.push(arguments)},h=d.getElementsByTagName('head')[0];
-          var c=d.createElement('script');o.api=new Array();c.async=true;c.type='text/javascript';
-          c.charset='utf-8';c.src='https://web-sdk.smartlook.com/recorder.js';h.appendChild(c);
-        })(document);
-        smartlook('init', 'bcd622dee28566231097b08a614f2ac2a4c1de02', {region: 'eu' });
-      </script>
+      <script data-host="https://microanalytics.io" data-dnt="false" src="https://microanalytics.io/js/script.js" id="ZwSg9rf6GA" async defer></script>
       <Component {...pageProps} />
     </Layout>
   )
