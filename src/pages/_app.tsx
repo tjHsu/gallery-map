49c39c9e182122a3021dc50ed54aefd9dedbbd3f
Ting <ting@Tings-MBP.fritz.box>
2023-03-22 15:40:55 +0100

    add vercel analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,14 @@
 import "../styles/globals.css";
 import type { AppType } from "next/dist/shared/lib/utils";
 import Layout from "../components/Layout";
+import { Analytics } from '@vercel/analytics/react';
 
 const MyApp: AppType = ({ Component, pageProps }) => {
 
   return (
     <Layout>
       <Component {...pageProps} />
+      <Analytics />
     </Layout>
   )
 };
