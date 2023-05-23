32e929e01ffccd84b7dbce2720afe4a7dec145ae
Ting <ting@Tings-MBP.fritz.box>
2023-05-23 21:02:17 +0200

    updated about us page, remove table head, and Twitter account link

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -2,22 +2,17 @@ import type { NextPage } from "next";
 
 const About: NextPage = () => {
   return (
-    <div className="ml-5">
+    <div className="ml-5 my-5">
       <div className="overflow-x-auto">
         <table className="table table-compact w-full">
           <tbody>
             <tr>
-              <td className="text-base">About tpgmap</td>
-              <td></td>
-            </tr>
-            <tr>
-              <td className="text-base">Information</td>
+              <td className="text-base">About</td>
               <td className="break-word whitespace-normal text-base">Your ultimate pocket guide to art galleries in Taipei.</td>
             </tr>
             <tr>
-              <td className="text-base align-top">Social networks</td>
+              <td className="text-base align-top">Social network</td>
               <td className="text-base">
-                <a href='https://twitter.com/tpegallerymap' target="_blank" rel="noreferrer">Twitter</a><br />
                 <a href='https://www.instagram.com/taipeigallerymap/' target="_blank" rel="noreferrer">Instagram</a>
               </td>
             </tr>
